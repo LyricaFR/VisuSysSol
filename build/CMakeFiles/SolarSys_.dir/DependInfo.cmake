7a55f442479ee3c63d32d6026f485e9897b3c339
DE JESUS MILITAR DYLAN <dyl.dejesusmilitar@gmail.com>
2023-11-22 16:38:36 +0100

    Added the include and src folders in the architecture

---
+++ build/CMakeFiles/SolarSys_.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/2ing2/dejesusmilitar/Documents/S1/Computer Graphics/VisuSysSol/SolarSys/src/events.cpp" "CMakeFiles/SolarSys_.dir/SolarSys/src/events.cpp.o" "gcc" "CMakeFiles/SolarSys_.dir/SolarSys/src/events.cpp.o.d"
+  "/home/2ing2/dejesusmilitar/Documents/S1/Computer Graphics/VisuSysSol/SolarSys/src/main.cpp" "CMakeFiles/SolarSys_.dir/SolarSys/src/main.cpp.o" "gcc" "CMakeFiles/SolarSys_.dir/SolarSys/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SolarSys_.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/2ing2/dejesusmilitar/Documents/S1/Computer Graphics/VisuSysSol/bin/SolarSys_"
+  "/home/2ing2/dejesusmilitar/Documents/S1/Computer Graphics/VisuSysSol/bin/SolarSys_.pdb"
+  "CMakeFiles/SolarSys_.dir/SolarSys/src/events.cpp.o"
+  "CMakeFiles/SolarSys_.dir/SolarSys/src/events.cpp.o.d"
+  "CMakeFiles/SolarSys_.dir/SolarSys/src/main.cpp.o"
+  "CMakeFiles/SolarSys_.dir/SolarSys/src/main.cpp.o.d"
+  "DUMMY_SolarSys/shaders/.DS_Store_SolarSys_"
+  "DUMMY_SolarSys/shaders/triangle.fs.glsl_SolarSys_"
+  "DUMMY_SolarSys/shaders/triangle.vs.glsl_SolarSys_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SolarSys_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
